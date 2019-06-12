4872bb854e5cb26ebfdddeceec4eff7efcf3d2cd
Mark Louie Espedido <espi@espidesigns.com>
2019-06-12 21:58:07 +0800

    Adds App component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,1 @@
-import React from 'react';import { render } from 'react-dom';import StorePicker from './components/StorePicker';
+import React from 'react';import { render } from 'react-dom';import StorePicker from './components/StorePicker';import App from './components/App';render(<App />, document.querySelector('#main'));
