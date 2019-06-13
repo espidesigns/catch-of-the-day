94c0ee814af3d2b16c6a0be72bbbcfef8301b79f
Mark Louie Espedido <espi@espidesigns.com>
2019-06-13 23:59:15 +0800

    Adds addToOrder method

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -13,7 +13,12 @@ class Fish extends React.Component {
 					<span className="price">{formatPrice(price)}</span>
 				</h3>
 				<p>{desc}</p>
-				<button disabled={!isAvailable}>{isAvailable ? 'Add To Cart' : 'Sold Out!'}</button>
+				<button
+					disabled={!isAvailable}
+					onClick={() => this.props.addToOrder(this.props.index)}
+				>
+					{isAvailable ? 'Add To Cart' : 'Sold Out!'}
+				</button>
 			</li>
 		)
 	}
