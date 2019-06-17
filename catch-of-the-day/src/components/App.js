6aa9eb8c9ef22a72d83dcb3082bcc2b603c7fd12
Mark Louie Espedido <espi@espidesigns.com>
2019-06-18 00:20:43 +0800

    Adds localStorage for persistent orders

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -20,6 +20,14 @@ class App extends React.Component {
 		});
 	}
 
+	componentDidUpdate() {
+		console.log(this.state.order);
+		localStorage.setItem(
+			this.props.match.params.storeId,
+			JSON.stringify(this.state.order)
+		);
+	}
+
 	componentWillUnmount() {
 		base.removeBinding(this.ref);
 	}
